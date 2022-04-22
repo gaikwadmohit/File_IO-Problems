ca1d233f1b2234be45307dd435f868e1df4888df
unknown <mohit16gaikwad@gmail.com>
2022-04-22 21:53:59 +0530

    Added : Pyroll service to store employee payroll into file

--- src/main/java/com/Bridgelabz/Day27/Employee_Payroll_Data.java
+++ src/main/java/com/Bridgelabz/Day27/Employee_Payroll_Data.java
@@ -1,43 +1,43 @@
 package com.Bridgelabz.Day27;
 
 public class Employee_Payroll_Data {
-	 public int employeeId;
-	    public String employeeName;
-	    public double salary;
 
-	    public Employee_Payroll_Data(int employeeId, String employeeName, double salary) {
-	        this.employeeId = employeeId;
-	        this.employeeName = employeeName;
-	        this.salary = salary;
-	    }
+    public int employeeId;
+    public String employeeName;
+    public double salary;
 
-	    public int getEmployeeId() {
-	        return employeeId;
-	    }
+    public Employee_Payroll_Data(int employeeId, String employeeName, double salary) {
+        this.employeeId = employeeId;
+        this.employeeName = employeeName;
+        this.salary = salary;
+    }
 
-	    public void setEmployeeId(int employeeId) {
-	        this.employeeId = employeeId;
-	    }
+    public int getEmployeeId() {
+        return employeeId;
+    }
 
-	    public String getEmployeeName() {
-	        return employeeName;
-	    }
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
 
-	    public void setEmployeeName(String employeeName) {
-	        this.employeeName = employeeName;
-	    }
+    public String getEmployeeName() {
+        return employeeName;
+    }
 
-	    public double getSalary() {
-	        return salary;
-	    }
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
 
-	    public void setSalary(double salary) {
-	        this.salary = salary;
-	    }
+    public double getSalary() {
+        return salary;
+    }
 
-	    @Override
-	    public String toString() {
-	        return "Id : " + employeeId + "\t" + "Name : " + employeeName + "\t" + "Salary : " + salary;
-	    }
-	}
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 
+    @Override
+    public String toString() {
+        return "Id : " + employeeId + "\t" + "Name : " + employeeName + "\t" + "Salary : " + salary;
+    }
+}
--- src/main/java/com/Bridgelabz/Day27/Employee_Payroll_Service.java
+++ src/main/java/com/Bridgelabz/Day27/Employee_Payroll_Service.java
@@ -4,41 +4,51 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Employee_Payroll_Service {
-	 Scanner SC = new Scanner(System.in);
+public class Employee_Payroll_Service<EmployeePayrollData> {
+	Scanner SC = new Scanner(System.in);
 
-	    public enum Ioservice {CONSALE_IO, FILE_IO, DB_IO, REST_IO}
+    public enum IOService {CONSALE_IO, FILE_IO, DB_IO, REST_IO, ioType}
 
-	    private List<Employee_Payroll_Data> employeePayrollList;
+    private List<Employee_Payroll_Data> employeePayrollList;
 
-	    public Employee_Payroll_Service() {
-	        this.employeePayrollList = new ArrayList<Employee_Payroll_Data>();
-	    }
+    public Employee_Payroll_Service() {
+        this.employeePayrollList = new employeePayrollList();
+    }
 
-	    public Employee_Payroll_Service(List<Employee_Payroll_Data> employeePayrollList) {
-	        this.employeePayrollList = employeePayrollList;
-	    }
+    public Employee_Payroll_Service(List<Employee_Payroll_Data> employeePayrollList) {
+        this.employeePayrollList = employeePayrollList;
+    }
 
-	    public static void main(String[] args) {
-	        ArrayList<Employee_Payroll_Data> employeePayrollList = new ArrayList<Employee_Payroll_Data>();
-	        Employee_Payroll_Service employeePayrollService = new Employee_Payroll_Service(employeePayrollList);
-	        Scanner consoleInputReader = new Scanner(System.in);
-	        employeePayrollService.readEmployeePayrollData(consoleInputReader);
-	        employeePayrollService.writeEmployeePayrollData();
-	    }
+    public static void main(String[] args) {
+        ArrayList<Employee_Payroll_Data> employeePayrollList = new ArrayList<>();
+        Employee_Payroll_Service employeePayrollService = new Employee_Payroll_Service(employeePayrollList);
+        Scanner consoleInputReader = new Scanner(System.in);
+        employeePayrollService.readEmployeePayrollData(consoleInputReader);
+        employeePayrollService.writeEmployeePayrollData(IOService.ioType);
+    }
 
-	    public void readEmployeePayrollData(Scanner consoleInputReader) {
-	        System.out.println("Enter Employee Id:");
-	        int employeeId = consoleInputReader.nextInt();
-	        System.out.println("Enter Employee Name:");
-	        String employeeName = consoleInputReader.next();
-	        System.out.println("Enter Employee Salary:");
-	        double employeeSalary = consoleInputReader.nextDouble();
-	        employeePayrollList.add(new Employee_Payroll_Data(employeeId, employeeName, employeeSalary));
-	    }
+    public void readEmployeePayrollData(Scanner consoleInputReader) {
+        System.out.println("Enter Employee Id:");
+        int employeeId = consoleInputReader.nextInt();
+        System.out.println("Enter Employee Name:");
+        String employeeName = consoleInputReader.next();
+        System.out.println("Enter Employee Salary:");
+        double employeeSalary = consoleInputReader.nextDouble();
+        employeePayrollList.add(new Employee_Payroll_Data(employeeId, employeeName, employeeSalary));
+    }
 
-	    public void writeEmployeePayrollData() {
-	        System.out.println("\nWriting Employee Payroll Roaster to Cansole\n" + employeePayrollList);
-	    }
-	}
+    public void writeEmployeePayrollData(IOService ioType) {
+        if (ioType.equals(IOService.CONSALE_IO)) {
+            for (EmployeePayrollData o : employeePayrollList)
+                System.out.println(o.toString());
+        } else if (ioType.equals(IOService.FILE_IO)) {
+            new Employee_PayrollFile_IO_Service().writeData(employeePayrollList);
+        }
+    }
 
+    public long countEnteries(IOService ioType) {
+        if (ioType.equals(IOService.FILE_IO))
+            return new Employee_PayrollFile_IO_Service().countEntries();
+        return 0;
+    }
+}
